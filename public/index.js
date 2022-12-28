82fb51329cba6dc7b994d9526277c83b2cfbddbd
Jonas Majauskas <jonasmajauskas1@gmail.com>
2022-12-27 21:56:28 -0500

    solution

--- public/index.js
+++ public/index.js
@@ -1,5 +1,40 @@
 async function main() {
 
+    let ctx = document.getElementById('myChart');
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
@@ -7,3 +42,14 @@ async function main() {
 }
 
 main()
+
+// async function getStockInfo() {
+//     let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,DIS,MSFT&interval=1min&apikey=ef292cea5c3d4066810559131fddd822')
+//     let result = await response.json()
+//     console.log(result)
+// }
+
+// getStockInfo()
+
+const {GME, DIS, MSFT} = mockData;
+console.log(Chart)
